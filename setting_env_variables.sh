97eb784888d16c6d2d3e6048554c839c025a0847
Yann Zerlaut <yann.zerlaut@gmail.com>
2019-11-06 15:53:31 +0100

    update of documentation

--- setting_env_variables.sh
+++ setting_env_variables.sh
@@ -4,6 +4,7 @@ output=$(python -c '
 import json, os
 from env_variables import SPREADSHEETS_ID, hbp_token_file, hbp_storage_token_file, VALIDATION_FRAMEWORK_INFOS
 
+print("echo -----TOKENS ------------------- ")
 if os.path.isfile(hbp_token_file): # defined in env_variables
    data1=json.load(open(hbp_token_file))
    print("echo [ok] the new HBP token is "+str(data1["access_token"][:10])+"..."+str(data1["access_token"][-10:]))
@@ -20,9 +21,12 @@ for key, val in SPREADSHEETS_ID.items():
 for key, val in VALIDATION_FRAMEWORK_INFOS.items():
     magic+="export %s=%s\n" % (key, val);
 magic+="export HBP_token="+str(data1["access_token"])+"\n";
-magic+="export HBP_STORAGE_TOKEN="+str(data2["auth"]["token"]["access_token"])+"" ; 
+magic+="export HBP_STORAGE_TOKEN="+str(data2["auth"]["token"]["access_token"])+"\n" ;
+magic+="export curation_url=https://docs.google.com/spreadsheets/d/%s/edit" % SPREADSHEETS_ID["MODEL_CURATION_SPREADSHEET"]	+"" ;
+print("echo -----SPREADSHEET ------------------- ")
+print("echo The spreadsheet for the model curation is avialble at the URL:")
+print("echo https://docs.google.com/spreadsheets/d/%s/edit" % SPREADSHEETS_ID["MODEL_CURATION_SPREADSHEET"])
 print(magic)')
-
 # echo "$output" # for debugging
 eval "$output"
 
