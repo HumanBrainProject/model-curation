eebb23d4331f10af17a47685350de80bdc9840ba
Yann Zerlaut <yann.zerlaut@gmail.com>
2020-01-22 17:56:06 +0100

    still need to understand the low DB

--- src/catalog_db.py
+++ src/catalog_db.py
@@ -1,25 +1,49 @@
 import sys, os, pathlib
 import numpy as np
 import pickle, pprint
+import time
 
-def load_models():
-    pkl_file = open(os.path.join(pathlib.Path(__file__).resolve().parents[1], 'db', 'CatalogDB.pkl'), 'rb')
-    models = pickle.load(pkl_file)
-    pkl_file.close()
+from fairgraph.client import KGClient
+from fairgraph.base import KGProxy
+from fairgraph.brainsimulation import ModelProject #, AbstractionLevel, BrainStructure, CellularTarget, License, ModelFormat, ModelScope, Publication, StudyTarget, FileBundle
+
+
+def load_models(client):
+    
+    models = ModelProject.list(client)
+    
     return models
 
-# here we store a list of models that were tests and wree not intended to be released
-SET_OF_EXCEPTIONS = [
-    'fs_morphologies',
-    'str-fs-161205_FS1-BE37A-20180212',
-    'msn_d2_all-5-20170517',
-    '',
-]
+def load_model_instances(client):
+
+    MODEL_INSTANCES = []
+    
+    models = ModelProject.list(client)
+    
+    for model in models:
+        # print('----------------------------------------')
+        # print('- %s ' % model.name)
+        if type(model.instances) is list:
+            for modelI in model.instances:
+                MODEL_INSTANCES.append(modelI.resolve(client))
+        elif type(model.instances) is KGProxy:
+            minst_proxy = model.instances
+            MODEL_INSTANCES.append(minst_proxy.resolve(client))
+        else:
+            print('Ignoring %s @ %s' % (model.name, model.date_created))
+            pass # we don't care about models without specific version
+
+        
+    return MODEL_INSTANCES
+
 
 if __name__=='__main__':
-    models = load_models()
-    print(models[0].keys())
-    # for model in models:
-    #     print('----------------------------------------')
-    #     print('- %s ' % model['name'])
-    #     print('  * %s' % model['description'])
+    
+    client = KGClient(os.environ["HBP_token"])
+    # models = load_models(client)
+
+    MODEL_INSTANCES = load_model_instances(client)
+
+    DATES = np.array([time.mktime(minst.timestamp.timetuple()) for minst in MODEL_INSTANCES])
+    for j, i in enumerate(np.argsort(DATES)):
+        print(j, ') ', MODEL_INSTANCES[i].name, ' --', MODEL_INSTANCES[i].timestamp)
